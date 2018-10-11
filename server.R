2c19d698901d21d2c6ad13d531d16342b1f21117
Nelson Gonzabato <42792514+GonzabatoNelson@users.noreply.github.com>
2018-10-11 11:32:55 +0800

    First Shiny App on Iris Dataset.

---
+++ server.R
@@ -0,0 +1,19 @@
+library(shiny)
+# Define server logic 
+shinyServer(function(input, output) {
+   
+  output$irisplot <- renderPlot({
+    shh<-suppressMessages
+    shh(library(tidyverse))
+    shh(library(lubridate))
+    #Some shiny data
+    var1<-as.numeric(input$Variable)
+    ggplot(iris,aes(iris[,var1]))+geom_histogram(fill=input$radio,alpha=input$alpha,
+                                                 binwidth = input$binwidth,col="black")+
+      scale_fill_manual(values=input$radio)
+  
+    
+    
+  })
+  
+})
